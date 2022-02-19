11724a9f66b714f48910f841d6c22002de77db8c
deepakhangingpanda <deepak.hangingpanda55@gmail.com>
2022-02-19 23:00:44 +0530

    todo Initilized

---
+++ src/components/TodoInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import "./TodoInput.css";
+
+const TodoInput = ({ createTodo }) => {
+    const [task, setTask] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task === "") {
+            alert("Please enter  todo");
+        }
+        else {
+            createTodo(task);
+            setTask("");
+
+        }
+
+    };
+    return (
+        <form className="TodoInput" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Enter Task"
+                id='task'
+                name="task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+            />
+            <button>Add Todo</button>
+        </form>
+    )
+
+}
+
+export default TodoInput
